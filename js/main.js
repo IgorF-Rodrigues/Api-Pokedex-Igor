40a1962adebfa7ac55ee4ca694a0f6b8599dd05d
IgorF-Rodrigues <107426768+IgorF-Rodrigues@users.noreply.github.com>
2023-09-23 07:55:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@ let geracao = 0;
 let id = 1;
 
 async function carregaAPI(id, geracao) {
+    const listaPokemon = [];
+
     for(id; id <= geracao; id++) {
         let api = `https://pokeapi.co/api/v2/pokemon/${id}`
         try {
             let link = await fetch(api);
             var pokemon = await link.json();
 
-            criarTemplate(id, pokemon);
+            listaPokemon.push(pokemon);
         } catch {
             throw new Error(`Não foi possivel buscar o pokémon do id ${id}`)
-        }
-        
+        }       
     };
+    criarTemplate(listaPokemon);
 };
